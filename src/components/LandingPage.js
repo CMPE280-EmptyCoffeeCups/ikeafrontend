e1c1130ee97df565350f16f68bf3331a942aef6f
Gaurav Chodwadia <gauravchodwadia@gmail.com>
2017-09-15 02:21:01 -0700

    Initial Commit + Landing Page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+
+class LandingPage extends Component{
+    render(){
+        return(
+            <img
+                className="landing-page-img"
+                src={'./images/desktop/gen/landing_desktop.jpg'}
+            />
+        )
+
+    }
+}
+
+
+export default LandingPage;
